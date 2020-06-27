40d61a349ccd9080f48d2378a4cadf400cb30053
james wu <jwu_nyc@yahoo.com>
2020-06-27 19:15:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,7 +18,7 @@ def hopper
     programmer_hash[:grace_hopper]
 end
 
-\
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -127,3 +127,4 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  end
